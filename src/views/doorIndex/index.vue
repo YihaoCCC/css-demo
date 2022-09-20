8f370911459ca631d67b4f856e99e80ba4807e83
YihaoCCC <yihao168830@163.com>
2022-09-20 23:47:48 +0800

    🌈 style(门户): 新增欢迎文字

--- src/views/doorIndex/index.vue
+++ src/views/doorIndex/index.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="door">
         <div class="fixContent">
+            <div class="welcome">
+                <span>W</span>
+                <span>E</span>
+                <span>L</span>
+                <span>C</span>
+                <span>O</span>
+                <span>M</span>
+                <span>E</span>
+            </div>
             <div class="footer">
                 往下滑动
             </div>
