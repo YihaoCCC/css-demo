93e360dc4d60767d6ba9ae392eb8890126fae43f
奕-Sugon <yihao168830@163.com>
2022-09-23 14:46:36 +0800

    ✨ feat(APP): 头部更新，全局版权通知

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   // 打开vite内网地址
   server: {
     host: '0.0.0.0'
