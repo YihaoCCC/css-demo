0d19c9b792e2dda3a1a9e3d6def146c41ca5f157
YihaoCCC <yhao88@126.com>
2023-04-05 12:31:03 +0800

    🌈 style(profile): 样式完成

--- src/views/profile/profile.vue
+++ src/views/profile/profile.vue
@@ -12,7 +12,59 @@
             <div class="info" >
                 <h2 :class="change ? 'showChange' : ''">{{ text }}</h2>
                 <div class="differentNav" :class="change ? 'showChange' : ''">
-                    <button v-if="showInfo === 0" @click="goUIHome">goUIHome</button>
+                    <button v-if="showInfo === 0" class="learn-more" @click="goUIHome">
+                        <span class="circle" aria-hidden="true">
+                        <span class="icon arrow"></span>
+                        </span>
+                        <span class="button-text">Go UIHome</span>
+                    </button>
+                    <div v-if="showInfo === 1" class="language">
+                        <img src="https://github-readme-stats.vercel.app/api/top-langs?username=YihaoCCC&show_icons=true&count_private=true&layout=compact&hide_border=true&langs_count=10&title_color=dd6687" alt="">
+                        <a class="cta" target="_blank" href="https://github.com/YihaoCCC">
+                            <svg viewBox="0 0 13 10" height="10px" width="15px">
+                                <path d="M1,5 L11,5"></path>
+                                <polyline points="8 1 12 5 8 9"></polyline>
+                            </svg>
+                            <span>Go My Git</span>
+                           
+                            <svg t="1663491437959" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                            p-id="2379" width="32" height="32" >
+                                <path
+                                    d="M512 12.64c-282.752 0-512 229.216-512 512 0 226.208 146.72 418.144 350.144 485.824 25.6 4.736 35.008-11.104 35.008-24.64 0-12.192-0.48-52.544-0.704-95.328-142.464 30.976-172.512-60.416-172.512-60.416-23.296-59.168-56.832-74.912-56.832-74.912-46.464-31.776 3.52-31.136 3.52-31.136 51.392 3.616 78.464 52.768 78.464 52.768 45.664 78.272 119.776 55.648 148.992 42.56 4.576-33.088 17.856-55.68 32.512-68.48-113.728-12.928-233.28-56.864-233.28-253.024 0-55.904 20-101.568 52.768-137.44-5.312-12.896-22.848-64.96 4.96-135.488 0 0 43.008-13.76 140.832 52.48a491.296 491.296 0 0 1 128.16-17.248c43.488 0.192 87.328 5.888 128.256 17.248 97.728-66.24 140.64-52.48 140.64-52.48 27.872 70.528 10.336 122.592 5.024 135.488 32.832 35.84 52.704 81.536 52.704 137.44 0 196.64-119.776 239.936-233.792 252.64 18.368 15.904 34.72 47.04 34.72 94.816 0 68.512-0.608 123.648-0.608 140.512 0 13.632 9.216 29.6 35.168 24.576C877.472 942.624 1024 750.784 1024 524.64c0-282.784-229.248-512-512-512z"
+                                    p-id="2380"></path>
+                            </svg>
+                           
+                        </a>
+                    </div>
+                    <div v-if="showInfo === 2" class="card">
+                        <div class="image">
+                            <img src="../../assets/WechatApp.jpg" alt="">
+                        </div>
+                        <div class="info">
+                            <div>
+                                <p class="name">
+                                   《 一图 》
+                                </p>
+                                <p class="function">
+                                    Front-end dev 
+                                </p>
+                            </div>
+                            <div class="stats">
+                                <p class="flex flex-col">
+                                    Articles
+                                    <span class="state-value">
+                                        34
+                                    </span>
+                                </p>
+                                <p class="flex">
+                                    Followers
+                                    <span class="state-value">
+                                        455
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -42,7 +94,6 @@ const handleImgSrc = async(num)=>{
         m = await import('../../assets/app.svg');
     }
     imgUrl.value = m.default;
-    console.log(123)
 };
 watch(floor, () => {
     change.value = true
@@ -53,7 +104,7 @@ watch(floor, () => {
         } else if (floor.value === 1) {
             text.value = 'Welcome To My Digital Garden'
         } else {
-            text.value = 'Welcome To My We Chat Mini programs'
+            text.value = 'Welcome To My WeChat Mini programs'
         }
         handleImgSrc(floor.value)
     }, 400);
